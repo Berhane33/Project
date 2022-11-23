a1b3466cda3a074329a7cf7a12e82763f9f1f399
Jacob Messou <jacob.messou@gmail.com>
2022-11-23 00:03:22 -0800

    donation section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,105 @@
-AOS.init();
+let donations = 0;
+let selected = false;
+let selId = 0;
+//Onclick function to highlight selected donation amount
+function setValue(element){
+  let val = element;
+  if(!selected){
+    val.style.background = "#82d14b";
+    selId = val;
+    selected = true;
+  }else{
+    selId.style.background = "#262c36";
+    val.style.background = "#82d14b";
+    selId = val;
+  }
+}
 
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
+function validateCVC() {
+  const cvc = document.getElementById("cvc");
+  const cvcreg = /\d{3,4}/;
+  return matchExists(cvcreg, cvc.value);
+}
 
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
+function matchExists(regex, value){
+  let mat = value.match(regex);
+  const matchExists = mat != null;
+  return (matchExists && mat[0].length == value.length);
+}
+function validateCC(){
+  const cc = document.getElementById("cc");
+  const ccreg = /\d{16}/;
+  return matchExists(ccreg, cc.value);
+}
+function validateName(){
+  const name = document.getElementById("name");
+  const namereg = /[a-zA-Z]*\s?[a-zA-Z]*/;
+  return matchExists(namereg, name.value);
+}
+function validateDate(){
+  const date = document.getElementById("date");
+  const datereg = /(1[0-2]|0[1-9]|\d)\/(1[7-9]|[2-9]\d)/;
+  return matchExists(datereg, date.value);
+}
+function validate(){
+   const indicator = document.getElementsByClassName("form-indicator")[0];
+  const cvc = validateCVC();
+  const cc = validateCC();
+  const date = validateDate();
+  const name = validateName();
+  
+  const allAreValid = cvc && cc && date && name;
+  
+  if(allAreValid){
+    switch(Number(selId.id)) {
+    case 1:
+        donations+=5;
+        break;
+    case 2:
+        donations+=10;
+        break;
+    case 3:
+        donations+=20;
+        break;
+    case 4:
+        donations+=40;
+        break;
+    case 5:
+        donations+=100;
+        break;
+    case 6:
+        donations+=250;
+        break;
+    case 7:
+        donations+=1000;
+        break;
+    default:
+        break;
+                }
+    const donation = document.getElementById("donation");
+    donation.innerHTML = "";
+    donation.innerHTML = "$"+donations+" won in the last 24 hours";
+}else{
+  if(!cc){
+    const ccError = document.getElementById("cc");
+    ccError.style.color = "red";
+  }
+  if(!cvc){
+    const cvcError = document.getElementById("cvc");
+    cvcError.style.color = "red";
+  }
+  if(!date){
+    const dateError = document.getElementById("date");
+    dateError.style.color = "red";
+  }
+  if(!name){
+    const nameError = document.getElementById("name");
+    nameError.style.color = "red";
+  }
+}
+  //   donations += 
+  //   indicator.style.background ="blue";
+  // }else{
+  //   indicator.style.background ="red";
+  // }
+}
